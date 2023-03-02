8d1e629dd609b7e1db090c445a228a15f5113363
André Torres <andre.torres@tecnico.ulisboa.pt>
2023-03-02 17:33:07 +0000

    test(domain): compose tests for the remove method of QuestionStats
    Close #32
    This is part of story #20

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/RemoveQuestionStatsTest.groovy
@@ -0,0 +1,40 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Teacher
+import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser;
+
+@DataJpaTest
+class RemoveQuestionStatsTest extends SpockTest {
+    def teacherDashboard
+
+    def setup() {
+        createExternalCourseAndExecution()
+
+        def teacher = new Teacher(USER_1_NAME, USER_1_USERNAME, USER_1_EMAIL, false, AuthUser.Type.TECNICO)
+        teacher.addCourse(externalCourseExecution)
+        userRepository.save(teacher)
+
+        teacherDashboard = new TeacherDashboard(externalCourseExecution, teacher)
+        teacherDashboardRepository.save(teacherDashboard)
+    }
+
+    def "remove a questionStats"() {
+        given: " a quizStats"
+        def questionStats = new QuestionStats(externalCourseExecution, teacherDashboard)
+        questionStatsRepository.save(questionStats)
+
+        when: "the questionStats is removed"
+        questionStats.remove()
+
+        then: "the teacher dashboard and the course execution don't have any QuestionStats"
+        teacherDashboard.getQuestionStats().isEmpty()
+        externalCourseExecution.getQuestionStats() == null
+    }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
+}
