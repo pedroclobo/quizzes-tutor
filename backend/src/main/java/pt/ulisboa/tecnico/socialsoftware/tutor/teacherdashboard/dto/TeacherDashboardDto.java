f4762875e9f7fcf4bd0bbe2273248a884e76fed2
Henrique Silva <henrique.d.silva@tecnico.ulisboa.pt>
2023-03-10 14:24:16 +0000

    feat: first attribute added to teacherDashboardDto, regarding studentStats
    This is part of issue #38
    This is part of story #35

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/dto/TeacherDashboardDto.java
@@ -1,11 +1,16 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.dto;
 
-import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDashboard;
+import java.util.stream.Collectors;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.*;
+import java.util.List;
+
 
 public class TeacherDashboardDto {
     private Integer id;
     private Integer numberOfStudents;
-
+    private List<Integer> numOfStudents;
+    
     public TeacherDashboardDto() {
     }
 
@@ -13,6 +18,10 @@ public class TeacherDashboardDto {
         this.id = teacherDashboard.getId();
         // For the number of students, we consider only active students
         this.numberOfStudents = teacherDashboard.getCourseExecution().getNumberOfActiveStudents();
+
+        this.numOfStudents = teacherDashboard.getStudentStats().stream()
+        .map(StudentStats::getNumStudents)
+        .collect(Collectors.toList());
     }
 
     public Integer getId() {
@@ -27,6 +36,14 @@ public class TeacherDashboardDto {
         return numberOfStudents;
     }
 
+    public List<Integer> getNumOfStudents() {
+        return numOfStudents;
+    }
+
+    public void setNumOfStudents(List<Integer> numOfStudents) {
+        this.numOfStudents = numOfStudents;
+    }
+
     public void setNumberOfStudents(Integer numberOfStudents) {
         this.numberOfStudents = numberOfStudents;
     }
@@ -36,6 +53,7 @@ public class TeacherDashboardDto {
         return "TeacherDashboardDto{" +
                 "id=" + id +
                 ", numberOfStudents=" + this.getNumberOfStudents() +
+                ", numOfStudents=" + this.getNumOfStudents() +
                 "}";
     }
 }
