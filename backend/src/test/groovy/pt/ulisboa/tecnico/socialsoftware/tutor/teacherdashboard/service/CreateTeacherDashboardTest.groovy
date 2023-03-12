af33557e0ca3d08b706ca4e63e7cbf250c674312
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2023-03-12 13:44:14 +0000

    test: compose tests for the association of the 3 latest course executions
    Close #42
    This is part of story #35, #36, #37

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/CreateTeacherDashboardTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/CreateTeacherDashboardTest.groovy
@@ -7,6 +7,8 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Teacher
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Course;
+import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution;
 import spock.lang.Unroll
 
 @DataJpaTest
@@ -93,23 +95,125 @@ import spock.lang.Unroll
     }
 
     def "create a dashboard with the teacher associated to one course execution"() {
-        expect:
-        false
+        given: "a teacher in a course execution"
+        teacher.addCourse(externalCourseExecution)
+
+        when: "a dashboard is created"
+        teacherDashboardService.createTeacherDashboard(externalCourseExecution.getId(), teacher.getId())
+
+        then: "the associated statistics have been created"
+        def result = teacherDashboardRepository.findAll().get(0)
+
+        result.getQuizStats().size() == 1
+        result.getStudentStats().size() == 1
+        result.getQuestionStats().size() == 1
+
+        result.getQuizStats().get(0).getCourseExecution().getId() == externalCourseExecution.getId()
+        result.getStudentStats().get(0).getCourseExecution().getId() == externalCourseExecution.getId()
+        result.getQuestionStats().get(0).getCourseExecution().getId() == externalCourseExecution.getId()
     }
 
     def "create a dashboard with the teacher associated to two course executions"() {
-        expect:
-        false
+        given: "a teacher in two course executions"
+        def courseExecution2 = new CourseExecution(externalCourse, COURSE_2_ACRONYM, COURSE_2_ACADEMIC_TERM, Course.Type.TECNICO, LOCAL_DATE_TODAY)
+        courseExecutionRepository.save(courseExecution2)
+
+        teacher.addCourse(externalCourseExecution)
+        teacher.addCourse(courseExecution2)
+
+        when: "a dashboard is created"
+        teacherDashboardService.createTeacherDashboard(externalCourseExecution.getId(), teacher.getId())
+
+        then: "the associated statistics have been created"
+        def result = teacherDashboardRepository.findAll().get(0)
+
+        result.getQuizStats().size() == 2
+        result.getStudentStats().size() == 2
+        result.getQuestionStats().size() == 2
+
+        result.getQuizStats().get(0).getCourseExecution().getId() == externalCourseExecution.getId()
+        result.getStudentStats().get(0).getCourseExecution().getId() == externalCourseExecution.getId()
+        result.getQuestionStats().get(0).getCourseExecution().getId() == externalCourseExecution.getId()
+
+        result.getQuizStats().get(1).getCourseExecution().getId() == courseExecution2.getId()
+        result.getStudentStats().get(1).getCourseExecution().getId() == courseExecution2.getId()
+        result.getQuestionStats().get(1).getCourseExecution().getId() == courseExecution2.getId()
     }
 
     def "create a dashboard with the teacher associated with three course executions"() {
-        expect:
-        false
+        given: "a teacher in three course executions"
+        def courseExecution2 = new CourseExecution(externalCourse, COURSE_2_ACRONYM, COURSE_2_ACADEMIC_TERM, Course.Type.TECNICO, LOCAL_DATE_TODAY)
+        courseExecutionRepository.save(courseExecution2)
+
+        def courseExecution3 = new CourseExecution(externalCourse, COURSE_3_ACRONYM, COURSE_3_ACADEMIC_TERM, Course.Type.TECNICO, LOCAL_DATE_TODAY)
+        courseExecutionRepository.save(courseExecution3)
+
+        teacher.addCourse(externalCourseExecution)
+        teacher.addCourse(courseExecution2)
+        teacher.addCourse(courseExecution3)
+
+        when: "a dashboard is created"
+        teacherDashboardService.createTeacherDashboard(externalCourseExecution.getId(), teacher.getId())
+
+        then: "the associated statistics have been created"
+        def result = teacherDashboardRepository.findAll().get(0)
+
+        result.getQuizStats().size() == 3
+        result.getQuizStats().get(0).getCourseExecution().getId() == externalCourseExecution.getId()
+        result.getQuizStats().get(1).getCourseExecution().getId() == courseExecution2.getId()
+        result.getQuizStats().get(2).getCourseExecution().getId() == courseExecution3.getId()
+
+        result.getStudentStats().size() == 3
+        result.getStudentStats().get(0).getCourseExecution().getId() == externalCourseExecution.getId()
+        result.getStudentStats().get(1).getCourseExecution().getId() == courseExecution2.getId()
+        result.getStudentStats().get(2).getCourseExecution().getId() == courseExecution3.getId()
+
+        result.getQuestionStats().size() == 3
+        result.getQuestionStats().get(0).getCourseExecution().getId() == externalCourseExecution.getId()
+        result.getQuestionStats().get(1).getCourseExecution().getId() == courseExecution2.getId()
+        result.getQuestionStats().get(2).getCourseExecution().getId() == courseExecution3.getId()
     }
 
     def "create a dashboard with the teacher associated with five course executions"() {
-        expect:
-        false
+        given: "a teacher in five course executions"
+        def courseExecution2 = new CourseExecution(externalCourse, COURSE_2_ACRONYM, COURSE_2_ACADEMIC_TERM, Course.Type.TECNICO, LOCAL_DATE_TODAY)
+        courseExecutionRepository.save(courseExecution2)
+
+        def courseExecution3 = new CourseExecution(externalCourse, COURSE_3_ACRONYM, COURSE_3_ACADEMIC_TERM, Course.Type.TECNICO, LOCAL_DATE_TODAY)
+        courseExecutionRepository.save(courseExecution3)
+
+        def courseExecution4 = new CourseExecution(externalCourse, COURSE_4_ACRONYM, COURSE_4_ACADEMIC_TERM, Course.Type.TECNICO, LOCAL_DATE_TODAY)
+        courseExecutionRepository.save(courseExecution4)
+
+        def courseExecution5 = new CourseExecution(externalCourse, COURSE_5_ACRONYM, COURSE_5_ACADEMIC_TERM, Course.Type.TECNICO, LOCAL_DATE_TODAY)
+        courseExecutionRepository.save(courseExecution5)
+
+        teacher.addCourse(externalCourseExecution)
+        teacher.addCourse(courseExecution2)
+        teacher.addCourse(courseExecution3)
+        teacher.addCourse(courseExecution4)
+        teacher.addCourse(courseExecution5)
+
+        when: "a dashboard is created"
+        teacherDashboardService.createTeacherDashboard(externalCourseExecution.getId(), teacher.getId())
+
+        then: "the associated statistics have been created"
+        def result = teacherDashboardRepository.findAll().get(0)
+
+        result.getQuizStats().size() == 3
+        result.getQuizStats().get(0).getCourseExecution().getId() == externalCourseExecution.getId()
+        result.getQuizStats().get(1).getCourseExecution().getId() == courseExecution2.getId()
+        result.getQuizStats().get(2).getCourseExecution().getId() == courseExecution3.getId()
+
+        result.getStudentStats().size() == 3
+        result.getStudentStats().get(0).getCourseExecution().getId() == externalCourseExecution.getId()
+        result.getStudentStats().get(1).getCourseExecution().getId() == courseExecution2.getId()
+        result.getStudentStats().get(2).getCourseExecution().getId() == courseExecution3.getId()
+
+        result.getQuestionStats().size() == 3
+        result.getQuestionStats().get(0).getCourseExecution().getId() == externalCourseExecution.getId()
+        result.getQuestionStats().get(1).getCourseExecution().getId() == courseExecution2.getId()
+        result.getQuestionStats().get(2).getCourseExecution().getId() == courseExecution3.getId()
     }
 
     @TestConfiguration
