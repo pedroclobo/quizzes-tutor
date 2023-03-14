e02f4b581a10d955f95118f4617e1b12be45cfc0
Gonçalo Nunes <goncaloinunes@tecnico.ulisboa.pt>
2023-03-14 15:00:00 +0000

    test: compose tests for removeTeacherDashboard in TeacherDashboardService
    Close #48
    This is part of stories #35 #36 #37

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/RemoveTeacherDashboardTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/RemoveTeacherDashboardTest.groovy
@@ -8,6 +8,11 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain.TeacherDa
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Teacher
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Course;
+import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution;
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.repository.QuestionStatsRepository;
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.repository.QuizStatsRepository;
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.repository.StudentStatsRepository;
 import spock.lang.Unroll
 
 @DataJpaTest
@@ -26,6 +31,13 @@ class RemoveTeacherDashboardTest extends SpockTest {
         def dashboard = new TeacherDashboard(externalCourseExecution, teacher)
         teacherDashboardRepository.save(dashboard)
         return dashboard
+    }
+
+    def createCourseExecution(Course course, String academicTerm) {
+        def courseExecution = new CourseExecution(course, COURSE_1_ACRONYM, academicTerm, Course.Type.TECNICO, LOCAL_DATE_TODAY)
+        courseExecutionRepository.save(courseExecution)
+
+        return courseExecution
     }
 
     def "remove a dashboard"() {
@@ -64,6 +76,38 @@ class RemoveTeacherDashboardTest extends SpockTest {
 
         where:
         dashboardId << [null, 10, -1]
+    }
+
+    def "the dashboard is removed from the teacher"() {
+        given: "a dashboard"
+        def dashboard = createTeacherDashboard()
+
+        when: "the user removes the dashboard"
+        teacherDashboardService.removeTeacherDashboard(dashboard.getId())
+
+        then: "the dashboard is removed from the teacher"
+        teacher.getDashboards().size() == 0
+    }
+
+    def "the associated statistics are deleted"() {
+        given: "a teacher in three course executions"
+        def courseExecution1 = createCourseExecution(externalCourse, "1º Semestre 2021/2022")
+        def courseExecution2 = createCourseExecution(externalCourse, "1º Semestre 2020/2021")
+        def courseExecution3 = createCourseExecution(externalCourse, "1º Semestre 2019/2020")
+
+        teacher.addCourse(courseExecution1)
+        teacher.addCourse(courseExecution2)
+        teacher.addCourse(courseExecution3)
+
+        when: "a dashboard is created and than removed"
+        def result = teacherDashboardService.createTeacherDashboard(courseExecution1.getId(), teacher.getId())
+
+        teacherDashboardService.removeTeacherDashboard(result.getId())
+
+        then: "the associated statistics are deleted"
+        questionStatsRepository.findAll().size() == 0
+        quizStatsRepository.findAll().size() == 0
+        studentStatsRepository.findAll().size() == 0   
     }
 
     @TestConfiguration
