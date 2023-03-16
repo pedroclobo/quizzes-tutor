e2fac6c4ca0beb09ae109aba4b7c2d23305ddd03
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2023-03-16 16:45:31 +0000

    test: add test case where there are newer course executions
    The latest 3 course executions include the current course execution and
    the two other course executions that immediately proceed the main one.
    This test case ensures that these are the course executions that get the
    new stats.
    
    This is part of task #42
    This is part of story #35, #36, #37

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/CreateTeacherDashboardTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/service/CreateTeacherDashboardTest.groovy
@@ -444,6 +444,58 @@ import spock.lang.Unroll
         teacherDashboard.getQuestionStats().get(0).getCourseExecution().getId() == courseExecution.getId()
     }
 
+    def "create a dashboard when there are more recent course execution than the one associated with the dashboard"() {
+        given: "a teacher in five course executions"
+        def courseExecution1 = createCourseExecution(externalCourse, "1º Semestre 2017/2018")
+        def courseExecution2 = createCourseExecution(externalCourse, "1º Semestre 2018/2019")
+        def courseExecution3 = createCourseExecution(externalCourse, "1º Semestre 2019/2020")
+        def courseExecution4 = createCourseExecution(externalCourse, "1º Semestre 2020/2021")
+        def courseExecution5 = createCourseExecution(externalCourse, "1º Semestre 2021/2022")
+
+        teacher.addCourse(courseExecution1)
+        teacher.addCourse(courseExecution2)
+        teacher.addCourse(courseExecution3)
+        teacher.addCourse(courseExecution4)
+        teacher.addCourse(courseExecution5)
+
+        when: "a dashboard is created"
+        def dashboard = teacherDashboardService.createTeacherDashboard(courseExecution3.getId(), teacher.getId())
+
+        then: "the quiz stats have been created and have the correct values"
+        dashboard.getNumQuizzes() == [0, 0, 0]
+        dashboard.getUniqueQuizzesSolved() == [0, 0, 0]
+        dashboard.getAverageQuizzesSolved() == [0f, 0f, 0f]
+
+        and: "the student stats have the correct values"
+        dashboard.getNumOfStudents() == [0, 0, 0]
+        dashboard.getNumMore75CorrectQuestions() == [0, 0, 0]
+        dashboard.getNumAtLeast3Quizzes() == [0, 0, 0]
+
+        and: "the question stats have the correct values"
+        dashboard.getNumAQuestionsAvailable() == [0, 0, 0]
+        dashboard.getUniqueQuestionsAnswered() == [0, 0, 0]
+        dashboard.getAverageQuestionsAnswered() == [0f, 0f, 0f]
+
+        and: "the course executions associated with the quiz stats are the most recent"
+        def teacherDashboard = teacherDashboardRepository.findAll().get(0)
+        teacherDashboard.getQuizStats().size() == 3
+        teacherDashboard.getQuizStats().get(0).getCourseExecution().getId() == courseExecution3.getId()
+        teacherDashboard.getQuizStats().get(1).getCourseExecution().getId() == courseExecution2.getId()
+        teacherDashboard.getQuizStats().get(2).getCourseExecution().getId() == courseExecution1.getId()
+
+        and: "the course executions associated with the student stats are the most recent"
+        teacherDashboard.getStudentStats().size() == 3
+        teacherDashboard.getStudentStats().get(0).getCourseExecution().getId() == courseExecution3.getId()
+        teacherDashboard.getStudentStats().get(1).getCourseExecution().getId() == courseExecution2.getId()
+        teacherDashboard.getStudentStats().get(2).getCourseExecution().getId() == courseExecution1.getId()
+
+        and: "the course executions associated with the question stats are the most recent"
+        teacherDashboard.getQuestionStats().size() == 3
+        teacherDashboard.getQuestionStats().get(0).getCourseExecution().getId() == courseExecution3.getId()
+        teacherDashboard.getQuestionStats().get(1).getCourseExecution().getId() == courseExecution2.getId()
+        teacherDashboard.getQuestionStats().get(2).getCourseExecution().getId() == courseExecution1.getId()
+    }
+
     @TestConfiguration
     static class LocalBeanConfiguration extends BeanConfiguration {}
 }
