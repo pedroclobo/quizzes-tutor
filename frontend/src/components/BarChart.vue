c4a3f6df9208323f4d0f31f28784123542dcc549
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2023-04-03 19:28:38 +0100

    feat: create bar chart component
    Close #67
    This is part of story #55

---
+++ frontend/src/components/BarChart.vue
@@ -0,0 +1,59 @@
+<template>
+  <Bar :chartData="data" :chartOptions="options" />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs/legacy';
+@Component({
+  components: {
+    Bar,
+  },
+})
+export default class BarChart extends Vue {
+  @Prop() readonly labels!: string[];
+  @Prop() readonly datasetLabels!: string[];
+  @Prop() readonly datasetData!: number[][];
+  data = {};
+  chartOptions = {
+    responsive: true,
+  };
+  created() {
+    ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+	this.updateData();
+  }
+  updateData() {
+    this.data = {
+    labels: this.labels,
+      datasets: [
+        {
+          label: this.datasetLabels[0],
+          backgroundColor: '#c0392b',
+          data: this.datasetData[0],
+        },
+        {
+          label: this.datasetLabels[1],
+          backgroundColor: '#2980b9',
+          data: this.datasetData[1],
+        },
+        {
+          label: this.datasetLabels[2],
+          backgroundColor: '#1abc9c',
+          data: this.datasetData[2],
+        }
+      ],
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss" />
