07be659708950244ba0c56f0f7b7d3461cdd3e86
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2023-04-12 23:18:24 +0100

    style: fix formatting
    This is part of task #56, #57, #58
    This is part of story #55

--- frontend/tests/e2e/specs/dashboard/teacherStats.js
+++ frontend/tests/e2e/specs/dashboard/teacherStats.js
@@ -1,70 +1,70 @@
 function populate2023CE() {
-    cy.createAndAddCourseExecutionToDemoTeacher("1 Semestre 2023/2024");
+  cy.createAndAddCourseExecutionToDemoTeacher("1 Semestre 2023/2024");
 
-    cy.createAndAddQuizToCourseExecution("Quiz 1", "1 Semestre 2023/2024");
-    cy.createAndAddQuizToCourseExecution("Quiz 2", "1 Semestre 2023/2024");
-    cy.createAndAddQuizToCourseExecution("Quiz 3", "1 Semestre 2023/2024");
+  cy.createAndAddQuizToCourseExecution("Quiz 1", "1 Semestre 2023/2024");
+  cy.createAndAddQuizToCourseExecution("Quiz 2", "1 Semestre 2023/2024");
+  cy.createAndAddQuizToCourseExecution("Quiz 3", "1 Semestre 2023/2024");
 
-    cy.createAndAddStudentToCourseExecution("Student 1", "1 Semestre 2023/2024");
-    cy.createAndAddStudentToCourseExecution("Student 2", "1 Semestre 2023/2024");
-    cy.createAndAddStudentToCourseExecution("Student 3", "1 Semestre 2023/2024");
+  cy.createAndAddStudentToCourseExecution("Student 1", "1 Semestre 2023/2024");
+  cy.createAndAddStudentToCourseExecution("Student 2", "1 Semestre 2023/2024");
+  cy.createAndAddStudentToCourseExecution("Student 3", "1 Semestre 2023/2024");
 
-    cy.createAndAddQuestionToQuiz("Quiz 1");
-    cy.createAndAddQuestionToQuiz("Quiz 2");
+  cy.createAndAddQuestionToQuiz("Quiz 1");
+  cy.createAndAddQuestionToQuiz("Quiz 2");
 
-    cy.createAndAddQuizAnswer("Quiz 1", "Student 1");
-    cy.createAndAddQuizAnswer("Quiz 2", "Student 1");
-    cy.createAndAddQuizAnswer("Quiz 3", "Student 1");
+  cy.createAndAddQuizAnswer("Quiz 1", "Student 1");
+  cy.createAndAddQuizAnswer("Quiz 2", "Student 1");
+  cy.createAndAddQuizAnswer("Quiz 3", "Student 1");
 
-    cy.addQuestionAnswer(1, 1);
+  cy.addQuestionAnswer(1, 1);
 }
 
 function populate2022CE() {
-    cy.createAndAddCourseExecutionToDemoTeacher("1 Semestre 2022/2023");
+  cy.createAndAddCourseExecutionToDemoTeacher("1 Semestre 2022/2023");
 
-    cy.createAndAddQuizToCourseExecution("Quiz 4", "1 Semestre 2022/2023");
-    cy.createAndAddQuizToCourseExecution("Quiz 5", "1 Semestre 2022/2023");
+  cy.createAndAddQuizToCourseExecution("Quiz 4", "1 Semestre 2022/2023");
+  cy.createAndAddQuizToCourseExecution("Quiz 5", "1 Semestre 2022/2023");
 
-    cy.createAndAddStudentToCourseExecution("Student 4", "1 Semestre 2022/2023");
-    cy.createAndAddStudentToCourseExecution("Student 5", "1 Semestre 2022/2023");
+  cy.createAndAddStudentToCourseExecution("Student 4", "1 Semestre 2022/2023");
+  cy.createAndAddStudentToCourseExecution("Student 5", "1 Semestre 2022/2023");
 
-    cy.createAndAddQuestionToQuiz("Quiz 4");
-    cy.createAndAddQuestionToQuiz("Quiz 5");
+  cy.createAndAddQuestionToQuiz("Quiz 4");
+  cy.createAndAddQuestionToQuiz("Quiz 5");
 }
 
 function populate2019CE() {
   cy.createAndAddCourseExecutionToDemoTeacher("1 Semestre 2019/2020");
-  
+
   cy.createAndAddQuizToCourseExecution("Quiz 6", "1 Semestre 2019/2020");
   cy.createAndAddQuizToCourseExecution("Quiz 7", "1 Semestre 2019/2020");
-  
+
   cy.createAndAddQuestionToQuiz("Quiz 6");
   cy.createAndAddQuestionToQuiz("Quiz 7");
-  
+
   cy.createAndAddStudentToCourseExecution("Student 6", "1 Semestre 2019/2020");
-  
+
   cy.createAndAddQuizAnswer("Quiz 6", "Student 6");
 }
 
 function removeQuizzes() {
-    cy.deleteQuizzesFromCourseExecution("1 Semestre 2019/2020");
-    cy.deleteQuizzesFromCourseExecution("1 Semestre 2022/2023");
-    cy.deleteQuizzesFromCourseExecution("1 Semestre 2023/2024");
+  cy.deleteQuizzesFromCourseExecution("1 Semestre 2019/2020");
+  cy.deleteQuizzesFromCourseExecution("1 Semestre 2022/2023");
+  cy.deleteQuizzesFromCourseExecution("1 Semestre 2023/2024");
 }
 
 function removeCourseExecutions() {
-    cy.removeCourseExecution("1 Semestre 2019/2020");
-    cy.removeCourseExecution("1 Semestre 2022/2023");
-    cy.removeCourseExecution("1 Semestre 2023/2024");
+  cy.removeCourseExecution("1 Semestre 2019/2020");
+  cy.removeCourseExecution("1 Semestre 2022/2023");
+  cy.removeCourseExecution("1 Semestre 2023/2024");
 }
 
 function removeAllStudents() {
-    cy.removeStudent("Student 1");
-    cy.removeStudent("Student 2");
-    cy.removeStudent("Student 3");
-    cy.removeStudent("Student 4");
-    cy.removeStudent("Student 5");
-    cy.removeStudent("Student 6");
+  cy.removeStudent("Student 1");
+  cy.removeStudent("Student 2");
+  cy.removeStudent("Student 3");
+  cy.removeStudent("Student 4");
+  cy.removeStudent("Student 5");
+  cy.removeStudent("Student 6");
 }
 
 function visualTestChart(filename) {
