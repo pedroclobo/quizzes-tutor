df6910b3cf72cdfcbd08fa0fadd848eed98950c9
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2023-04-14 13:48:46 +0100

    test: 2019 end-to-end test is now accessing correct dashboard
    This is part of task #58
    This is part of story #55

--- frontend/tests/e2e/specs/dashboard/teacherStats.js
+++ frontend/tests/e2e/specs/dashboard/teacherStats.js
@@ -182,20 +182,20 @@ describe('TeacherStats', () => {
     cy.demoTeacherLogin();
 
     cy.get('[data-cy="changeCourseButton"]').click();
-    cy.get('[data-cy="selectCourseButton"]').eq(0).click();
+    cy.get('[data-cy="selectCourseButton"]').eq(2).click();
     cy.get('[data-cy="dashboardMenuButton"]').click();
 
-    cy.get('[data-cy="numStudents"]').should('have.text', '3');
+    cy.get('[data-cy="numStudents"]').should('have.text', '1');
     cy.get('[data-cy="numMore75CorrectQuestions"]').should('have.text', '0');
-    cy.get('[data-cy="numAtLeast3Quizzes"]').should('have.text', '1');
+    cy.get('[data-cy="numAtLeast3Quizzes"]').should('have.text', '0');
 
-    cy.get('[data-cy="numQuizzes"]').should('have.text', '3');
-    cy.get('[data-cy="numUniqueAnsweredQuizzes"]').should('have.text', '3');
+    cy.get('[data-cy="numQuizzes"]').should('have.text', '2');
+    cy.get('[data-cy="numUniqueAnsweredQuizzes"]').should('have.text', '1');
     cy.get('[data-cy="averageQuizzesSolved"]').should('have.text', '1');
 
     cy.get('[data-cy="numQuestions"]').should('have.text', '2');
-    cy.get('[data-cy="answeredQuestionsUnique"]').should('have.text', '1');
-    cy.get('[data-cy="averageQuestionsAnswered"]').should('have.text', '1');
+    cy.get('[data-cy="answeredQuestionsUnique"]').should('have.text', '0');
+    cy.get('[data-cy="averageQuestionsAnswered"]').should('have.text', '0');
 
     cy.get('[data-cy="barChart"]').should('not.be.visible');
 
